e5a264437c48067e0ea65c5436d2a0d402a011d1
igor <igoracciolylima@gmail.com>
2026-05-12 09:13:15 -0300

    teste pagina dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/register/register.jsx';
 import LoginPage from './pages/login/login.jsx';
+import Dashboard from './pages/dashboard/dashboard.jsx';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
